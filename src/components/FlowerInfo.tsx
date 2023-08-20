9556c132f1163d44cca2eeb5dc4879d0504f2eba
Mykyta <nikitochkaaaaaa@gmail.com>
2023-08-20 13:58:10 +0300

    Flower info parameters added

--- src/components/FlowerInfo.tsx
+++ src/components/FlowerInfo.tsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 const FlowerInfo = () => {
   return (
-    <div className="flowerinfo">
-      <span>FlowerInfo</span>
+    <div className="flower_info">
+      <span>Flower Name</span>
+      <div className="basic_info">
+        <div className="info">
+          <div className="icon">💧</div>
+          <div className="water_info">water</div>
+        </div>
+        <div className="info">
+          <div className="icon">💨</div>
+          <div className="humidity_info">humidity</div>
+        </div>
+        <div className="info">
+          <div className="icon">🔆</div>
+          <div className="sunlight_info">sunlight</div>
+        </div>
+      </div>
+
     </div>
   );
 };
--- src/components/FlowersList.tsx
+++ src/components/FlowersList.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FlowersList = () => {
   return (
-    <div className="flowerslist">
+    <div className="flowers_list">
       <div className="searchbar">
         <input type="text" placeholder="Search plant"/>
       </div>
